e750ca4cb325766f18a64da3c690dd0ac535917b
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-02-08 16:15:45 +0800

    lowpass denoiser!!

--- dsp/03-denoise/dsp.c
+++ dsp/03-denoise/dsp.c
@@ -3,13 +3,29 @@
 #include "jack.h"
 #include <stdio.h>
 
+static float alpha = 0.98f;
+static float comp_alpha = 0.02f;
+static float last;
+
 void process_wave(float* in, float* out, size_t nframe)
 {
+	// y[n] = alpha * y[n-1] + (1-alpha) * x[n]
+	for (; nframe--; in++, out++)
+		last = *out = alpha * last + comp_alpha * *in;
 }
 
 void process_midi(size_t frame, uint8_t ev[3])
 {
-	printf("%lu: %2.2hhx %2.2hhx %2.2hhx\n", frame, ev[0], ev[1], ev[2]);
+	if (ev[0] == 0xb0 && ev[1] == 0x7f) {	// controller no. 127
+		alpha = 1.0f - ev[2] / 127.0f;	//-make alpha goes slower when
+		alpha = 1.0f - alpha * alpha;	// reaching 1
+		comp_alpha = 1.0f - alpha;		//-complementary of alpha
+
+		printf("alpha = %4.4f", alpha);
+		if (ev[2] == 0) puts("  original sound");
+		else if (ev[2] == 0x7f) puts("  complete silence");
+		else putchar('\n');
+	}
 }
 
 
