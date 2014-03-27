5bd03a578af7e84c1ed722bf28a85bd1ddcde001
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-03-27 21:54:05 +0800

    gst: testvideo

---
+++ gstreamer/01-gst/gst.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gst/gst.h>
+
+#define ANY  (void*)
+int main()
+{
+	gst_init(NULL, NULL);
+
+	GstElement* pipeline = gst_pipeline_new("test");
+
+	GstElement* video = gst_element_factory_make("videotestsrc", "src");
+	g_object_set(video, "pattern", 0, NULL);
+	gst_bin_add(ANY pipeline, video);
+
+	GstElement* display = gst_element_factory_make("xvimagesink", "sink");
+	gst_bin_add(ANY pipeline, display);
+
+	gst_element_link(video, display);
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+	sleep(2);
+	return 0;
+}
+
