4e76e7e4ba829674de091cdda78d5a6c5d016cc8
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-03-27 22:43:28 +0800

    unified way to create pipeline

--- gstreamer/01-gst/gst.c
+++ gstreamer/01-gst/gst.c
@@ -7,13 +7,14 @@ int main()
 {
 	gst_init(NULL, NULL);
 
-	GstElement* pipeline = gst_pipeline_new("test");
+	//GstElement* pipeline = gst_pipeline_new("test");
+	GstElement* pipeline = gst_element_factory_make("pipeline", NULL);
 
-	GstElement* video = gst_element_factory_make("videotestsrc", "src");
+	GstElement* video = gst_element_factory_make("videotestsrc", NULL);
 	g_object_set(video, "pattern", 0, NULL);
 	gst_bin_add(ANY pipeline, video);
 
-	GstElement* display = gst_element_factory_make("xvimagesink", "sink");
+	GstElement* display = gst_element_factory_make("xvimagesink", NULL);
 	gst_bin_add(ANY pipeline, display);
 
 	gst_element_link(video, display);
