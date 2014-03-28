c768d646c40263662b52b7654b4df3190d441045
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-03-28 16:50:32 +0800

    opstack: more api

--- opstack/gst.c
+++ opstack/gst.c
@@ -14,7 +14,13 @@ static void pushpointer(lua_State* L, void* p);
 static void init(lua_State* L);
 
 
-static int l_list(lua_State* L);
+static int l_elements(lua_State* L);
+static int l_element(lua_State* L);
+static int l_set(lua_State* L);
+static int l_add(lua_State* L);
+static int l_link(lua_State* L);
+static int l_play(lua_State* L);
+static int l_stop(lua_State* L);
 
 
 
@@ -24,7 +30,16 @@ int luaopen_gst(lua_State* L)
 
 #define FUNC(NAME) { #NAME, &l_##NAME }
 	static const luaL_Reg lib[] = {
-		FUNC(list),
+		FUNC(elements),
+		FUNC(element),
+		FUNC(set),
+		FUNC(add),
+		FUNC(link),
+		FUNC(play),
+		FUNC(stop),
+		//FUNC(unlink),
+		//FUNC(state),
+		//FUNC(states),
 		{ NULL, NULL },
 	};
 #undef FUNC
@@ -62,27 +77,12 @@ static void pushpointer(lua_State* L, void* p)
 static void init(lua_State* L)
 {
 	gst_init(NULL, NULL);
-/*
-	GList* elems = gst_element_factory_list_get_elements(
-			GST_ELEMENT_FACTORY_TYPE_ANY, 0);
-	{
-		void $(GstElementFactory* factory)
-		{
-			printf("%-20.20s [%-19.19s] %-37.37s\n",
-				gst_plugin_feature_get_name(ANY factory),
-				gst_element_factory_get_metadata(factory, "klass"),
-				gst_element_factory_get_metadata(factory, "long-name"));
-		}
-		g_list_foreach(elems, ANY $, NULL);
-	}
-	gst_plugin_feature_list_free(elems);
-*/
 }
 
 
 
 
-static int l_list(lua_State* L)
+static int l_elements(lua_State* L)
 {
 	lua_newtable(L);
 
@@ -109,5 +109,54 @@ static int l_list(lua_State* L)
 	gst_plugin_feature_list_free(elems);
 
 	return 1;
+}
+
+static int l_element(lua_State* L)
+{
+	const char* name = luaL_checkstring(L, 1);
+	GstElement* elem = gst_element_factory_make(name, NULL);
+	pushpointer(L, elem);
+	return 1;
+}
+
+static int l_set(lua_State* L)
+{
+	/*
+	GstElement* elem = checkpointer(L, 1);
+	const char* k = luaL_checkstring(L, 2);
+	switch (lua_type(L, 3)) {
+	}
+	*/
+	return 0;
+}
+
+static int l_add(lua_State* L)
+{
+	GstBin* bin = checkpointer(L, 1);
+	GstElement* elem = checkpointer(L, 2);
+	gst_bin_add(bin, elem);
+	return 0;
+}
+
+static int l_link(lua_State* L)
+{
+	GstElement* src = checkpointer(L, 1);
+	GstElement* sink= checkpointer(L, 2);
+	gst_element_link(src, sink);
+	return 0;
+}
+
+static int l_play(lua_State* L)
+{
+	GstElement* elem= checkpointer(L, 1);
+	gst_element_set_state(elem, GST_STATE_PLAYING);
+	return 0;
+}
+
+static int l_stop(lua_State* L)
+{
+	GstElement* elem= checkpointer(L, 1);
+	gst_element_set_state(elem, GST_STATE_NULL);
+	return 0;
 }
 
--- opstack/test.lua
+++ opstack/test.lua
@@ -1,10 +1,31 @@
 
+local sleep = function(seconds)
+	local start = os.clock()
+	while os.clock()-start < seconds do end
+end
+
 local gst = require 'gst'
 
-elements = gst.list()
+-- print all elements
+local elements = gst.elements()
+local fmt = "%s\n    %-76.76s\n    %-76.76s\n    %-76.76s\n"
 for name,prop in pairs(elements) do
-	for k,v in pairs(prop) do
-		print(("%s.%s = %q"):format(name, k, v))
-	end
+	print(fmt:format(name, prop.category, prop.longname, prop.description))
 end
+
+-- create a pipeline: videotestsrc ! xcimagesink
+local pipe = gst.element('pipeline')
+local src = gst.element('videotestsrc')
+local sink = gst.element('xvimagesink')
+gst.add(pipe, src)
+gst.add(pipe, sink)
+gst.link(src, sink)
+
+-- play the pipeline
+gst.play(pipe)
+sleep(1)
+gst.stop(pipe)
+sleep(0.5)
+gst.play(pipe)
+sleep(2)
 
