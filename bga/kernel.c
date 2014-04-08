5fd60a552af84be4ead5441eca7ba9f1e1118901
eXerigumo Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2014-04-08 14:51:52 +0800

    better architectured

--- bga/kernel.c
+++ bga/kernel.c
@@ -1,31 +1,4 @@
-
-#define PAGE_ALIGN			(1<<0)
-#define MEM_INFO			(1<<1)
-
-#define MAGIC				(0x1badb002)
-#define FLAGS				(PAGE_ALIGN | MEM_INFO)
-#define CHECKSUM			(-(MAGIC + FLAGS))
-
-#define UNUSED				__attribute__((unused))
-#define PACKED				__attribute__((packed))
-#define ANY					(void*)
-
-typedef unsigned int u32;
-typedef unsigned char u8;
-
-typedef struct Header
-{
-	u32 magic;
-	u32 flags;
-	u32 checksum;
-}
-Header;
-
-static UNUSED Header header = {
-	.magic = MAGIC,
-	.flags = FLAGS,
-	.checksum = CHECKSUM,
-};
+#include "util.h"
 
 typedef struct Glyph
 {
@@ -37,10 +10,9 @@ Glyph;
 
 static Glyph* text = ANY 0xb8000;
 
-void _start()
+void main()
 {
 	text[0].color = 0b0100;
 	text[0].ch = 'a';
-	while (1);
 }
 
---
+++ bga/util.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define UNUSED				__attribute__((unused))
+#define PACKED				__attribute__((packed))
+#define ANY					(void*)
+
+typedef unsigned int u32;
+typedef unsigned char u8;
+
